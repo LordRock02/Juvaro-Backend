786a71b62fbeed9b476887fd9d587166b7b9e6a5
roger gomez <rogergt02@gmail.com>
2025-06-11 15:44:04 -0500

    estructura del proyecto

---
+++ src/main/java/com/backend/juvaro/JuvaroBackendApplication.java
@@ -0,0 +1,13 @@
+package com.backend.juvaro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuvaroBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JuvaroBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/backend/juvaro/JuvaroBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.juvaro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuvaroBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
