51b2b26645d3d780966d06a048bc0fc139e0cea2
roger gomez <rogergt02@gmail.com>
2025-06-23 13:57:06 -0500

    agregada preautorizacion por rol producto

--- src/main/java/com/backend/juvaro/controllers/ProductoController.java
+++ src/main/java/com/backend/juvaro/controllers/ProductoController.java
@@ -9,6 +9,7 @@ import com.backend.juvaro.services.ProductoService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,26 +21,31 @@ public class ProductoController {
     private final ProductoService productoService;
 
     @GetMapping
+    @PreAuthorize("hasAnyRole('USER','ADMIN')")
     public ResponseEntity<List<ProductoDto>> getAllProductos() throws  BadRequestException {
         return new ResponseEntity<>(productoService.listarProductos(), HttpStatus.OK);
     }
 
     @PostMapping("/registrar")
+    @PreAuthorize("hasAnyRole('ADMIN')")
     public ResponseEntity<?> registrarProducto(@RequestBody RegistrarProductoRequest request) throws BadRequestException {
         return new ResponseEntity<>(productoService.registrarProducto(request), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasAnyRole('USER','ADMIN')")
     public ResponseEntity<ProductoDto> ObtenerRecurso(@PathVariable Long id) throws ResourceNotFoundException {
         return new ResponseEntity<>(productoService.buscarProductoPorId(id), HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN')")
     public ResponseEntity<?> eliminarProducto(@PathVariable Long id) throws ResourceNotFoundException {
         return new ResponseEntity<>(productoService.eliminarProducto(id),HttpStatus.OK);
     }
 
     @PutMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN')")
     public ResponseEntity<ProductoDto> modificarProducto(@PathVariable Long id, @RequestBody UpdateProductoRequest request) throws ResourceNotFoundException {
         return new ResponseEntity<>(productoService.ActualizarProducto(request,id),HttpStatus.OK);
     }
--- src/main/java/com/backend/juvaro/controllers/UsuarioController.java
+++ src/main/java/com/backend/juvaro/controllers/UsuarioController.java
@@ -13,6 +13,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import jakarta.validation.Valid;
@@ -26,7 +27,7 @@ public class UsuarioController {
     private final Logger LOGGER = LoggerFactory.getLogger(UsuarioService.class);
 
     @PostMapping("/registrar")
-    //@PreAuthorize("hasAnyRole('ADMIN')")
+    @PreAuthorize("hasAnyRole('ADMIN')")
     public ResponseEntity<?> registrarUsuario(@Valid @RequestBody RegistrarUsuarioRequest usuario) throws BadRequestException {
         LOGGER.info("Inicia registro");
         LOGGER.info("Usuario request: "+ JsonPrinter.toString(usuario));
@@ -34,25 +35,25 @@ public class UsuarioController {
     }
 
     @PutMapping("actualizar")
-    //@PreAuthorize("hasAnyRole('ADMIN')")
+    @PreAuthorize("hasAnyRole('ADMIN')")
     public ResponseEntity<UsuarioDto> actualizarUsuario(@Valid @RequestBody UpdateUsuarioRequest usuario) throws ResourceNotFoundException, BadRequestException {
         return new ResponseEntity<>(usuarioService.actualizarUsuario(usuario), HttpStatus.OK);
     }
 
     @GetMapping("/listar")
-    //@PreAuthorize("hasAnyRole('ADMIN')")
+    @PreAuthorize("hasAnyRole('ADMIN')")
     public ResponseEntity<List<UsuarioDto>> listarUsuarios() {
         return new ResponseEntity<>(usuarioService.listarUsuarios(), HttpStatus.OK);
     }
 
     @GetMapping("{id}")
-    //@PreAuthorize("hasAnyRole('ADMIN')")
+    @PreAuthorize("hasAnyRole('ADMIN')")
     public ResponseEntity<UsuarioDto> obtenerUsuarioPorId(@PathVariable Long id) throws ResourceNotFoundException {
         return new ResponseEntity<>(usuarioService.buscarUsuarioPorId(id), HttpStatus.OK);
     }
 
     @DeleteMapping("eliminar/{id}")
-    //@PreAuthorize("hasAnyRole('ADMIN')")
+    @PreAuthorize("hasAnyRole('ADMIN')")
     public ResponseEntity<?> eliminar(@PathVariable Long id) throws ResourceNotFoundException {
         return new ResponseEntity<>(usuarioService.eliminarUsuario(id), HttpStatus.OK);
     }
