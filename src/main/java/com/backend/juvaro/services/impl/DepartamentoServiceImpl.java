0c79e9315ae1a95de994f2baaf256e1cdac1b632
roger gomez <rogergt02@gmail.com>
2025-06-23 15:58:46 -0500

    cambios listar departamentos

--- src/main/java/com/backend/juvaro/services/impl/DepartamentoServiceImpl.java
+++ src/main/java/com/backend/juvaro/services/impl/DepartamentoServiceImpl.java
@@ -13,6 +13,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -28,6 +29,7 @@ public class DepartamentoServiceImpl implements DepartamentoService {
     private DepartamentoMapper departamentoMapper;
 
     @Override
+    @Transactional(readOnly = true)
     public List<DepartamentoDto> listarDepartamentos() throws BadRequestException {
         return departamentoRepository.findAll()
                 .stream()
